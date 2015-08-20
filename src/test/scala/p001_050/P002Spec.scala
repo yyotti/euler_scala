25b0098e01e6c520ac902427cd768b82d4d651db
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-20 17:31:17 +0900

    表現を修正

--- src/test/scala/p001_050/P002Spec.scala
+++ src/test/scala/p001_050/P002Spec.scala
@@ -5,27 +5,27 @@ import org.specs2.mutable._
 
 class P002Spec extends Specification {
   "P002#fib(a1, a2, max)" should {
-    "returns () if (a1, a2, max) = (1, 2, 0)" in {
+    "returns [] if (a1, a2, max) = (1, 2, 0)" in {
       P002.fib(1, 2, 0) must beEmpty
     }
 
-    "returns (1) if (a1, a2, max) = (1, 2, 1)" in {
+    "returns [1] if (a1, a2, max) = (1, 2, 1)" in {
       P002.fib(1, 2, 1) must beEqualTo(List(1))
     }
 
-    "returns (1, 2) if (a1, a2, max) = (1, 2, 2)" in {
+    "returns [1, 2] if (a1, a2, max) = (1, 2, 2)" in {
       P002.fib(1, 2, 2) must beEqualTo(List(1, 2))
     }
 
-    "returns (1, 2, 3) if (a1, a2, max) = (1, 2, 3)" in {
+    "returns [1, 2, 3] if (a1, a2, max) = (1, 2, 3)" in {
       P002.fib(1, 2, 3) must beEqualTo(List(1, 2, 3))
     }
 
-    "returns (1, 2, 3) if (a1, a2, max) = (1, 2, 4)" in {
+    "returns [1, 2, 3] if (a1, a2, max) = (1, 2, 4)" in {
       P002.fib(1, 2, 4) must beEqualTo(List(1, 2, 3))
     }
 
-    "returns (1, 2, 3, 5, 8, 13, 21, 34, 55, 89) if (a1, a2, max) = (1, 2, 90)" in {
+    "returns [1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if (a1, a2, max) = (1, 2, 90)" in {
       P002.fib(1, 2, 90) must beEqualTo(List(1, 2, 3, 5, 8, 13, 21, 34, 55, 89))
     }
   }
