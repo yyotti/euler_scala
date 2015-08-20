d1c76c5b5684b8507738e44fb3fb4acdccacc296
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-20 17:26:55 +0900

    Problem 3 への回答完了

--- src/main/scala/p001_050/P003.scala
+++ src/main/scala/p001_050/P003.scala
@@ -12,5 +12,16 @@ package project_euler
  * 600851475143 の素因数のうち最大のものを求めよ.
  */
 object P003 {
-  def solve(n: Long): Int = ???
+  def nums(start: Long, step: Int): Stream[Long] = start #:: nums(start + step, step)
+
+  val primes: Stream[Long] = 2L #:: nums(3, 2).filter { n => primes.takeWhile { _ <= math.sqrt(n) }.forall { n % _ != 0 } }
+
+  def primeFactors(n: Long): List[Long] =
+    if (n == 1) Nil
+    else primes.takeWhile { _ <= math.sqrt(n) }.find { n % _ == 0 } match {
+      case Some(k) => k :: primeFactors(n / k)
+      case _ => List(n)
+    }
+
+  def solve(n: Long): Long = primeFactors(n).last
 }
--- src/test/scala/p001_050/P003Spec.scala
+++ src/test/scala/p001_050/P003Spec.scala
@@ -4,7 +4,43 @@ import project_euler.P003
 import org.specs2.mutable._
 
 class P003Spec extends Specification {
-  "P003#solve(n)" should {
+  "P003#primes" should {
+    "first 10 terms are [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]" in {
+      P003.primes.take(10).toList must beEqualTo(List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29))
+    }
+  }
+
+  "P003#primeFactors(Long)" should {
+    "returns [] if n = 1" in {
+      P003.primeFactors(1) must beEmpty
+    }
+
+    "returns [2] if n = 2" in {
+      P003.primeFactors(2) must beEqualTo(List(2))
+    }
+
+    "returns [3] if n = 3" in {
+      P003.primeFactors(3) must beEqualTo(List(3))
+    }
+
+    "returns [2] if n = 4" in {
+      P003.primeFactors(4) must beEqualTo(List(2, 2))
+    }
+
+    "returns [2, 3] if n = 6" in {
+      P003.primeFactors(6) must beEqualTo(List(2, 3))
+    }
+
+    "returns [2, 2, 3] if n = 12" in {
+      P003.primeFactors(12) must beEqualTo(List(2, 2, 3))
+    }
+
+    "returns [5, 7, 13, 29] if n = 13195" in {
+      P003.primeFactors(13195) must beEqualTo(List(5, 7, 13, 29))
+    }
+  }
+
+  "P003#solve(Long)" should {
     "returns 29 if n = 13195" in {
       P003.solve(13195) must beEqualTo(29)
     }
