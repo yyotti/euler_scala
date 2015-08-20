b80558d0dcdded16b9099dc9e2f6d280b9895e61
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-20 17:32:09 +0900

    問題の日本語訳を修正

--- src/main/scala/p001_050/P001.scala
+++ src/main/scala/p001_050/P001.scala
@@ -3,11 +3,13 @@ package project_euler
 /**
  * Problem 1:
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
  * Find the sum of all the multiples of 3 or 5 below 1000.
  *
  * 問題 1:
- * 10未満の自然数のうち、3もしくは5の倍数を列挙すると、3,5,6,9である。その和は23である。
- * 1000未満の自然数のうち、3もしくは5の倍数の和を求めよ。
+ * 10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
+ *
+ * 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
  */
 object P001 {
   def solve(n: Int): Long =
