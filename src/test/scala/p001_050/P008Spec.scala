8ae7f37f4cb743625fa473b71a1bb0e60a8022b7
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-21 14:56:37 +0900

    Problem 8 への回答完了

--- src/test/scala/p001_050/P008Spec.scala
+++ src/test/scala/p001_050/P008Spec.scala
@@ -5,8 +5,8 @@ import org.specs2.mutable._
 
 class P008Spec extends Specification {
   "P008#solve(Int)" should {
-    "returns 3024 if n = 4" in {
-      P008.solve(4) must beEqualTo(3024)
+    "returns 5832 if n = 4" in {
+      P008.solve(4) must beEqualTo(5832)
     }
 
     "returns 23514624000 if n = 13" in {
