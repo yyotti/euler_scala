d7ba33f47484fb43bcaa84dacd470ab486aa60ec
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-21 14:47:07 +0900

    Problem 8 の準備

---
+++ src/test/scala/p001_050/P008Spec.scala
@@ -0,0 +1,16 @@
+package test.project_euler
+
+import project_euler.P008
+import org.specs2.mutable._
+
+class P008Spec extends Specification {
+  "P008#solve(Int)" should {
+    "returns 3024 if n = 4" in {
+      P008.solve(4) must beEqualTo(3024)
+    }
+
+    "returns 23514624000 if n = 13" in {
+      P008.solve(13) must beEqualTo(23514624000L)
+    }
+  }
+}
