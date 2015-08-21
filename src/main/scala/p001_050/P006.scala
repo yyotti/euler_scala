5ff61f1de69915ba3a8c426cf7a8385f6df583ec
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-21 14:12:38 +0900

    Problem 6 の準備

---
+++ src/main/scala/p001_050/P006.scala
@@ -0,0 +1,28 @@
+package project_euler
+
+/**
+ * Problem 6:
+ * The sum of the squares of the first ten natural numbers is,
+ *
+ * 1^2 + 2^2 + ... + 10^2 = 385
+ * The square of the sum of the first ten natural numbers is,
+ *
+ * (1 + 2 + ... + 10)^2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ *
+ * 問題 6:
+ * 最初の10個の自然数について, その二乗の和は,
+ *
+ * 1^2 + 2^2 + ... + 10^2 = 385
+ * 最初の10個の自然数について, その和の二乗は,
+ *
+ * (1 + 2 + ... + 10)^2 = 3025
+ * これらの数の差は 3025 - 385 = 2640 となる.
+ *
+ * 同様にして, 最初の100個の自然数について二乗の和と和の二乗の差を求めよ.
+ */
+object P006 {
+  def solve(n: Int): Long = ???
+}
---
+++ src/test/scala/p001_050/P006Spec.scala
@@ -0,0 +1,16 @@
+package test.project_euler
+
+import project_euler.P006
+import org.specs2.mutable._
+
+class P006Spec extends Specification {
+  "P006#solve(Int)" should {
+    "returns 2640 if n = 10" in {
+      P006.solve(10) must beEqualTo(2640)
+    }
+
+    "returns 25164150 if n = 100" in {
+      P006.solve(100) must beEqualTo(25164150)
+    }
+  }
+}
