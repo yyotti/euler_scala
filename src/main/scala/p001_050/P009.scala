3b19005db9e75c3f3a9a17e67e36830d95bc1544
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-22 14:21:36 +0900

    Problem 9 への回答完了

--- src/main/scala/p001_050/P009.scala
+++ src/main/scala/p001_050/P009.scala
@@ -20,5 +20,16 @@ package project_euler
  * これらの積 abc を計算しなさい.
  */
 object P009 {
-  def solve: Long = ???
+  def isPytagoreanTriplet(a: Long, b: Long, c: Long) = a * a + b * b == c * c
+
+  def solve: Long =
+    (1L to 1000).flatMap { a =>
+      ((a + 1) to (1000 - a - 1) / 2).map { b =>
+        (a, b, 1000 - a - b)
+      }
+    }.withFilter {
+      case (a, b, c) => isPytagoreanTriplet(a, b, c)
+    }.map {
+      case (a, b, c) => a * b * c
+    }.headOption.getOrElse(0)
 }
--- src/test/scala/p001_050/P009Spec.scala
+++ src/test/scala/p001_050/P009Spec.scala
@@ -4,6 +4,20 @@ import project_euler.P009
 import org.specs2.mutable._
 
 class P009Spec extends Specification {
+  "P009#isPytagoreanTriplet(Long, Long, Long)" should {
+    "returns false if (a, b, c) = (1, 1, 1)" in {
+      P009.isPytagoreanTriplet(1, 1, 1) must beFalse
+    }
+
+    "returns true if (a, b, c) = (3, 4, 5)" in {
+      P009.isPytagoreanTriplet(3, 4, 5) must beTrue
+    }
+
+    "returns true if (a, b, c) = (6, 8, 10)" in {
+      P009.isPytagoreanTriplet(6, 8, 10) must beTrue
+    }
+  }
+
   "P009#solve" should {
     "returns 31875000" in {
       P009.solve must beEqualTo(31875000)
