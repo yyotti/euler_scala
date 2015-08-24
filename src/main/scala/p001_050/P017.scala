450c60bd8535c829b7cb1f28c86fedd9cf920e06
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-24 11:59:04 +0900

    問題だけ作成、回答は後回し

---
+++ src/main/scala/p001_050/P017.scala
@@ -0,0 +1,22 @@
+package project_euler
+
+/**
+ * Problem 17:
+ * If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+ *
+ * If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+ *
+ *
+ * NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+ *
+ * 問題 17:
+ * 1 から 5 までの数字を英単語で書けば one, two, three, four, five であり, 全部で 3 + 3 + 5 + 4 + 4 = 19 の文字が使われている.
+ *
+ * では 1 から 1000 (one thousand) までの数字をすべて英単語で書けば, 全部で何文字になるか.
+ *
+ * 注: 空白文字やハイフンを数えないこと. 例えば, 342 (three hundred and forty-two) は 23 文字, 115 (one hundred and fifteen) は20文字と数える. なお, "and" を使用するのは英国の慣習.
+ */
+object P017 {
+  // FIXME 後回し
+  def solve(n: Int): Long = ???
+}
