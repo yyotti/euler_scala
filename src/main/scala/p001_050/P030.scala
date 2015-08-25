74c607a792a7fead19e84855ace9e9ada3a8bd03
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-25 19:56:36 +0900

    Problem 30 への回答完了

--- src/main/scala/p001_050/P030.scala
+++ src/main/scala/p001_050/P030.scala
@@ -26,5 +26,29 @@ package project_euler
  * 各桁を5乗した数の和が元の数と一致するような数の総和を求めよ.
  */
 object P030 {
-  def solve(n: Int): Long = ???
+  import commons._
+
+  def findMaxDigits(n: Int) =
+    from(1).takeWhile { k => digitCount((BigInt(9).pow(n) * k)) >= k }.last.toInt
+
+  /*
+   * aをk桁の数とすると、
+   *   a = a(k - 1)*10^(k - 1) + a(k - 2)*10^(k - 2) + ... + a1*10 + a0
+   * と表せる。ただし a(i) は 0 &le; a(i) &le; 9 の整数とし、0 &le; i &lt; k である。
+   *
+   * aの各桁をn乗した数の和をS(a)とすると、
+   *   S(a) = a(k - 1)^n + a(k - 2)^n + ... + a1^n + a0^n
+   * となるが、 0 &le; a(i) &le; 9 であることから、S(a)の最大値は
+   *   max(S(a)) = 9^n + 9^n + ... + 9^n + 9^n
+   *             = k*9^n
+   * である。max(S(a))の桁数をmとすれば、m &lt; n である場合は「各桁のn乗の和が元の
+   * 数と一致する」ことは有り得ない。
+   *
+   * また、1桁の数の場合、2乗して元の数と一致するのは1しかないが、1は除外するので
+   * 2桁以上の数でチェックすればよい。
+   */
+  def solve(n: Int): Long =
+    (2L to (BigInt(10).pow(findMaxDigits(n)) - 1).toLong).filter { k =>
+      digits(k).map { i => BigInt(i).pow(n) }.sum == k
+    }.sum
 }
--- src/test/scala/p001_050/P030Spec.scala
+++ src/test/scala/p001_050/P030Spec.scala
@@ -4,6 +4,24 @@ import project_euler.P030
 import org.specs2.mutable._
 
 class P030Spec extends Specification {
+  "P030#findMaxDigits(Int)" should {
+    "returns 3 if n = 2" in {
+      P030.findMaxDigits(2) must beEqualTo(3)
+    }
+
+    "returns 4 if n = 3" in {
+      P030.findMaxDigits(3) must beEqualTo(4)
+    }
+
+    "returns 5 if n = 4" in {
+      P030.findMaxDigits(4) must beEqualTo(5)
+    }
+
+    "returns 6 if n = 5" in {
+      P030.findMaxDigits(5) must beEqualTo(6)
+    }
+  }
+
   "P030#solve(Int)" should {
     "returns 19316 if n = 4" in {
       P030.solve(4) must beEqualTo(19316)
