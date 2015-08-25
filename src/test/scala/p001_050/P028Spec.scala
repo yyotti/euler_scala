b5879b2206917d6612cb42a9f805a5e739c0edfc
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-08-25 18:36:06 +0900

    Problem 28 への回答完了

--- src/test/scala/p001_050/P028Spec.scala
+++ src/test/scala/p001_050/P028Spec.scala
@@ -4,6 +4,20 @@ import project_euler.P028
 import org.specs2.mutable._
 
 class P028Spec extends Specification {
+  "P028#t(Int)" should {
+    "returns 0 if n = 1" in {
+      P028.t(1) must beEqualTo(0)
+    }
+
+    "returns 24 if n = 3" in {
+      P028.t(3) must beEqualTo(24)
+    }
+
+    "returns 76 if n = 5" in {
+      P028.t(5) must beEqualTo(76)
+    }
+  }
+
   "P028#solve(Int)" should {
     "returns 101 if n = 5" in {
       P028.solve(5) must beEqualTo(101)
