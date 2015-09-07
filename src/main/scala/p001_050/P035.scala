c40a09253c3d8dd5d9d501922db7c86d0bc62ecd
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-09-07 17:32:21 +0900

    Problem 35 への回答完了

--- src/main/scala/p001_050/P035.scala
+++ src/main/scala/p001_050/P035.scala
@@ -16,5 +16,25 @@ package project_euler
  * 100万未満の巡回素数はいくつあるか?
  */
 object P035 {
-  def solve(n: Int): Int = ???
+  import commons._
+
+  /**
+   * n が巡回素数であるためには、下記の全てを満たさなければならない。
+   * 1. n が素数であること
+   * 2. 桁を回転させたときに得られる数が素数であること
+   *
+   * 2の条件を満たすためには、全ての桁の数字が5以外の奇数でなければならない。
+   * ※偶数を含むと桁を回転させたらいつか偶数になり、5を含むといつか5の倍数になる。
+   */
+  def solve(n: Int): Int =
+    primes
+      .takeWhile { _ < n }
+      .filter { p =>
+        (p < 10 || !digits(p).exists { k => k % 2 == 0 || k == 5 }) &&
+        circulars(p).forall { isPrime }
+      }
+      .size
+
+  def circulars(n: Long): List[Long] =
+    (0 to (digitCount(n) - 1)).map { i => val ds = digits(n); ds.drop(i) ++: ds.take(i) }.map { _.mkString.toLong }.toList
 }
