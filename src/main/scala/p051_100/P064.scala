71b63305d56a230cdbada1ca678ed7d15638f681
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-09-21 14:33:22 +0900

    Problem 64 への回答完了

--- src/main/scala/p051_100/P064.scala
+++ src/main/scala/p051_100/P064.scala
@@ -91,5 +91,67 @@ package project_euler
  * N ≤ 10000 について奇数の周期をもつ平方根が何個あるか答えよ.
  */
 object P064 {
-  def solve(n: Int): Long = ???
+  import commons._
+
+  def continuedFractions(n: Int): Option[(Int, List[Int])] = {
+    def continuedPart(k: Int, l: Int, cache: Set[(Int, Int)]): List[Int] = {
+      if (cache.contains((k, l))) Nil
+      else {
+        val g = gcd(n - k * k, l).toInt
+        val d = (n - k * k) / g
+        val x = from(d, d).map { i => (k - i).abs.toInt }.find { i => math.pow(i, 2) > n }.get - d
+        val y = (x + k) / d
+
+        y :: continuedPart(x, d, cache + ((k, l)))
+      }
+    }
+
+    from(1).find { k => k * k >= n }.flatMap { k => if (k * k == n) None else Some(k.toInt - 1) } match {
+      case Some(a0) => Some((a0, continuedPart(a0, 1, Set[(Int, Int)]())))
+      case _ => None
+    }
+  }
+
+  /**
+   * nを2以上の自然数とする。
+   *   √n = a0 + 1 / (a1 + 1 / (a2 + 1 / (a3 + ...)))
+   * であり、省略形は
+   *   √n = [a0; (a1, a2, ... , ai)]  (iは0以上の整数)
+   * であるから、まずa0を考える。
+   *
+   * √n = a0 + √n - a0 の形に変形することが目的である。このとき、次のa1の計算を行う際に、
+   * 1/(√n - a0) = (√n + a0)/(n - (a0)^2) の形の変形を行うので、
+   *   (a0)^2がnを超えない最大のa0
+   * を求める。
+   *
+   * 次にa1,a2,...,aiを求めるために、下記の漸化式を考える。
+   *   li/(√n - ki) = aj + (√n - lj)/dj   (j = i + 1)
+   * ただし、l0 = 1, k0 = a0 である。
+   * まず左辺を
+   *   li(√n + ki)/(n - (ki)^2)
+   * と変形する。このとき、liと(n - (ki)^2)の最大公約数はliになるので、(n - (ki)^2)/li = d とおくと
+   *   li/(√n - ki) = li(√n + ki)/(n - (ki)^2) = (√n + k)/d
+   * となる。
+   * さらにここから
+   *   (√n + k)/d = aj + (√n - lj)/dj
+   * の変形を行う。右辺を通分して整理すると
+   *   (√n + k)/d = (√n + aj*dj - lj)/dj
+   * となるので、
+   *   dj = d、aj*dj - lj = k
+   * となるように、aj と lj を求めてやればよい。ただし、ljは
+   *   (lj)^2がnを超えない最大のlj
+   * とする。
+   *
+   * dj, lj が求まったら、漸化式で di = dj, li = lj として次の ai を求める。
+   * 処理過程で出現した(dj, lj)の組を記憶しておき、同じ組が出現したらそこまでを
+   * 周期とする。
+   */
+  def solve(n: Int): Long =
+    from(2)
+      .takeWhile { _ <= n }
+      .map { i => continuedFractions(i.toInt) }
+      .count {
+        case Some((_, ls)) => ls.size % 2 == 1
+        case None => false
+      }
 }
--- src/test/scala/p051_100/P064Spec.scala
+++ src/test/scala/p051_100/P064Spec.scala
@@ -4,6 +4,56 @@ import project_euler.P064
 import org.specs2.mutable._
 
 class P064Spec extends Specification {
+  "P064#continuedFractions(Int)" should {
+    "returns Some(1, [2]) if n = 2" in {
+      P064.continuedFractions(2) must beSome((1, List(2)))
+    }
+
+    "returns Some(1, [1, 2]) if n = 3" in {
+      P064.continuedFractions(3) must beSome((1, List(1, 2)))
+    }
+
+    "returns None if n = 4" in {
+      P064.continuedFractions(4) must beNone
+    }
+
+    "returns Some(2, [4]) if n = 5" in {
+      P064.continuedFractions(5) must beSome((2, List(4)))
+    }
+
+    "returns Some(2, [2, 4]) if n = 6" in {
+      P064.continuedFractions(6) must beSome((2, List(2, 4)))
+    }
+
+    "returns Some(2, [1, 1, 1, 4]) if n = 7" in {
+      P064.continuedFractions(7) must beSome((2, List(1, 1, 1, 4)))
+    }
+
+    "returns Some(2, [1, 4]) if n = 8" in {
+      P064.continuedFractions(8) must beSome((2, List(1, 4)))
+    }
+
+    "returns None if n = 9" in {
+      P064.continuedFractions(9) must beNone
+    }
+
+    "returns Some(3, [6]) if n = 10" in {
+      P064.continuedFractions(10) must beSome((3, List(6)))
+    }
+
+    "returns Some(3, [3, 6]) if n = 11" in {
+      P064.continuedFractions(11) must beSome((3, List(3, 6)))
+    }
+
+    "returns Some(3, [2, 6]) if n = 12" in {
+      P064.continuedFractions(12) must beSome((3, List(2, 6)))
+    }
+
+    "returns Some(3, [1, 1, 1, 1, 6]) if n = 13" in {
+      P064.continuedFractions(13) must beSome((3, List(1, 1, 1, 1, 6)))
+    }
+  }
+
   "P064#solve(Int)" should {
     "returns 4 if n = 13" in {
       P064.solve(13) must beEqualTo(4)
