c6278ac4401a7e9600c0746b690ac198d614d38b
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-01 10:51:17 +0900

    Problem 77 への回答完了

--- src/main/scala/p051_100/P077.scala
+++ src/main/scala/p051_100/P077.scala
@@ -24,5 +24,18 @@ package project_euler
  * 素数の和としての表し方が5000通り以上になる最初の数を求めよ.
  */
 object P077 {
-  def solve(n: Int): Long = ???
+  import commons._
+
+  def pi(n: Long, nums: List[Long]): Long = (n, nums) match {
+    case (0, _) => 1
+    case (_, Nil) => 0
+    case (_, x :: xs) if (n < x) => pi(n, xs)
+    case (_, x :: xs) => pi(n - x, nums) + pi(n, xs)
+  }
+
+  /**
+   * 基本的にはP076と同じで、表し方を順に計算していって最初に5000を超える数を返せばよい。
+   */
+  def solve(n: Int): Long =
+    from(2).find { i => pi(i, primes.takeWhile { _ < i }.toList.reverse) >= n }.get
 }
