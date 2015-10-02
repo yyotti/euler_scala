6fa678ea0108008ed11f808e503f24cfdd2e419e
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-02 17:07:30 +0900

    Problem 80 への回答完了

--- src/main/scala/p051_100/P080.scala
+++ src/main/scala/p051_100/P080.scala
@@ -19,5 +19,98 @@ package project_euler
  * 初めの100個の自然数の平方根のうち, 無理数について, それぞれの頭から100桁の数字を足した数の総和を求めよ.
  */
 object P080 {
-  def solve(n: Int): Long = ???
+  import commons._
+  import scala.language.implicitConversions
+
+  implicit def Int2Fraction(i: Int) = Fraction(i, 1)
+  implicit def BigInt2Fraction(i: BigInt) = Fraction(i, 1)
+
+  case class Fraction(n: BigInt, d: BigInt) {
+    lazy val lowestTerm = {
+      val k = gcd(n, d)
+      Fraction(n / k, d / k)
+    }
+
+    def +(that: Fraction): Fraction = {
+      val k = lcm(d, that.d)
+      val m = n * k / d + that.n * k / that.d
+
+      Fraction(m, k).lowestTerm
+    }
+
+    def *(that: Fraction): Fraction = {
+      Fraction(n * that.n, d * that.d).lowestTerm
+    }
+
+    def ~ : Fraction = Fraction(d, n)
+  }
+  object Fraction {
+    def apply(n: Fraction, f: Fraction): Fraction = n * f.~
+  }
+
+  def continuedFractions(n: Int): Option[(Int, List[Int])] = {
+    def continuedPart(k: Int, l: Int, cache: Set[(Int, Int)]): List[Int] = {
+      if (cache.contains((k, l))) Nil
+      else {
+        val g = gcd(n - k * k, l).toInt
+        val d = (n - k * k) / g
+        val x = from(d, d).map { i => (k - i).abs.toInt }.find { i => math.pow(i, 2) > n }.get - d
+        val y = (x + k) / d
+
+        y :: continuedPart(x, d, cache + ((k, l)))
+      }
+    }
+
+    from(1).find { k => k * k >= n }.flatMap { k => if (k * k == n) None else Some(k.toInt - 1) } match {
+      case Some(a0) => Some((a0, continuedPart(a0, 1, Set[(Int, Int)]())))
+      case _ => None
+    }
+  }
+
+  def div(n: BigInt, d: BigInt, count: Int): List[Int] = (n, d, count) match {
+    case (_, _, 0) => Nil
+    case (n, d, _) if n % d == 0 => (n / d).toInt :: Nil
+    case _ => (n / d).toInt :: div((n % d) * 10, d, count - 1)
+  }
+
+  def appr(a0: Int, part: List[Int], count: Int): List[Int] = {
+    val as = from(0).flatMap { _ => part }
+
+    def toFraction(nums: List[Int]): Fraction = nums match {
+      case Nil => 0
+      case x :: xs => Fraction(1, x + toFraction(xs))
+    }
+
+    from(1)
+      .map { k => val f = a0 + toFraction(as.take(k.toInt).toList); div(f.n, f.d, count) }
+      .sliding(2)
+      .find { ls => ls(0) == ls(1) }
+      .map { _(0) }
+      .get
+  }
+
+  /**
+   * P064より、自然数Nの整数にならない平方根は、連分数で表現できる。
+   *   √N = a0 + (1/a1 + (1/a2 + (1/a3 + ...)
+   * a1,a2,... はいつか循環するので、簡易表記として
+   *   √N = [a0;(a1,a2,...,ak)]
+   * √2を簡易表記すると
+   *   √2=[1;(2)]
+   * のようになる。
+   *
+   * √Nを小数表記するためには、√N ≒ m/n となる自然数(m,n)の組を見つけ、
+   * m/nを小数表記すればよい。ただし、今回は√Nの先頭から100桁までをとらなければ
+   * ならないので、最低でも先頭100桁が√Nと一致する(m,n)の組を探す必要がある。
+   *
+   * そこで、√Nの連分数の簡易表記を取得し、循環部分を1桁ずつ増やしながら(m,n)の組
+   * を見つけ、小数表記する。kを自然数として、この数列を
+   *   r(k) = a0 + (1/a1 + (1/a2 + ... + 1/ak)
+   * とすると、r(k) - r(k - 1) &le; 10^(-100) を満たすkを見つけられれば、
+   * √Nと先頭100桁が一致する近似値を求められる。
+   */
+  def solve(n: Int): Long =
+    (1 to n)
+      .flatMap { i => continuedFractions(i) }
+      .map { case (a0, part) => appr(a0, part, 100).sum }
+      .sum
 }
--- src/test/scala/p051_100/P080Spec.scala
+++ src/test/scala/p051_100/P080Spec.scala
@@ -1,9 +1,108 @@
 package test.project_euler
 
 import project_euler.P080
+import project_euler.P080.Fraction
 import org.specs2.mutable._
 
 class P080Spec extends Specification {
+  "P080#continuedFractions(Int)" should {
+    "returns Some(1, [2]) if n = 2" in {
+      P080.continuedFractions(2) must beSome((1, List(2)))
+    }
+
+    "returns Some(1, [1, 2]) if n = 3" in {
+      P080.continuedFractions(3) must beSome((1, List(1, 2)))
+    }
+
+    "returns None if n = 4" in {
+      P080.continuedFractions(4) must beNone
+    }
+
+    "returns Some(2, [4]) if n = 5" in {
+      P080.continuedFractions(5) must beSome((2, List(4)))
+    }
+
+    "returns Some(2, [2, 4]) if n = 6" in {
+      P080.continuedFractions(6) must beSome((2, List(2, 4)))
+    }
+
+    "returns Some(2, [1, 1, 1, 4]) if n = 7" in {
+      P080.continuedFractions(7) must beSome((2, List(1, 1, 1, 4)))
+    }
+
+    "returns Some(2, [1, 4]) if n = 8" in {
+      P080.continuedFractions(8) must beSome((2, List(1, 4)))
+    }
+
+    "returns None if n = 9" in {
+      P080.continuedFractions(9) must beNone
+    }
+
+    "returns Some(3, [6]) if n = 10" in {
+      P080.continuedFractions(10) must beSome((3, List(6)))
+    }
+
+    "returns Some(3, [3, 6]) if n = 11" in {
+      P080.continuedFractions(11) must beSome((3, List(3, 6)))
+    }
+
+    "returns Some(3, [2, 6]) if n = 12" in {
+      P080.continuedFractions(12) must beSome((3, List(2, 6)))
+    }
+
+    "returns Some(3, [1, 1, 1, 1, 6]) if n = 13" in {
+      P080.continuedFractions(13) must beSome((3, List(1, 1, 1, 1, 6)))
+    }
+  }
+
+  "P080#div(Int, Int, Int)" should {
+    "returns [] if (n, d, count) = (1, 2, 0)" in {
+      P080.div(1, 2, 0) must beEmpty
+    }
+
+    "returns [] if (n, d, count) = (3, 2, 0)" in {
+      P080.div(3, 2, 0) must beEmpty
+    }
+
+    "returns [2] if (n, d, count) = (2, 1, 3)" in {
+      P080.div(2, 1, 3) must beEqualTo(List(2))
+    }
+
+    "returns [1, 5] if (n, d, count) = (3, 2, 3)" in {
+      P080.div(3, 2, 3) must beEqualTo(List(1, 5))
+    }
+
+    "returns [3, 3, 3, 3, 3, 3] if (n, d, count) = (10, 3, 6)" in {
+      P080.div(10, 3, 6) must beEqualTo(List(3, 3, 3, 3, 3, 3))
+    }
+  }
+
+  "P080#appr(Int, Stream[Int], Int)" should {
+    "returns [1, 4, 1, 4, 2, 1, 3, 5, 6] if (a0, part, count) = (1, [2], 9)" in {
+      P080.appr(1, List(2), 9) must beEqualTo(List(1, 4, 1, 4, 2, 1, 3, 5, 6))
+    }
+
+    "returns [1, 4, 1, 4, 2, 1, 3, 5, 6, ...] if (a0, part, count) = (1, [2], 99)" in {
+      val expected = List(
+        1, 4, 1, 4, 2, 1, 3, 5, 6, 2,
+        3, 7, 3, 0, 9, 5, 0, 4, 8, 8,
+        0, 1, 6, 8, 8, 7, 2, 4, 2, 0,
+        9, 6, 9, 8, 0, 7, 8, 5, 6, 9,
+        6, 7, 1, 8, 7, 5, 3, 7, 6, 9,
+        4, 8, 0, 7, 3, 1, 7, 6, 6, 7,
+        9, 7, 3, 7, 9, 9, 0, 7, 3, 2,
+        4, 7, 8, 4, 6, 2, 1, 0, 7, 0,
+        3, 8, 8, 5, 0, 3, 8, 7, 5, 3,
+        4, 3, 2, 7, 6, 4, 1, 5, 7
+      )
+      P080.appr(1, List(2), 99) must beEqualTo(expected)
+    }
+
+    "returns [1, 7, 3, 2, 0, 5, 0, 8] if (a0, part, count) = (1, [1, 2], 8)" in {
+      P080.appr(1, List(1, 2), 8) must beEqualTo(List(1, 7, 3, 2, 0, 5, 0, 8))
+    }
+  }
+
   "P080#solve(Int)" should {
     "returns 475 if n = 2" in {
       P080.solve(2) must beEqualTo(475)
