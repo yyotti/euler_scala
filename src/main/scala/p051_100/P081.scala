0511860ecc5876bb1610fe5ec4543ee90d677cdf
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-05 11:56:40 +0900

    [P081] ダイクストラ法の実装による高速化

--- src/main/scala/p051_100/P081.scala
+++ src/main/scala/p051_100/P081.scala
@@ -29,21 +29,84 @@ package project_euler
  */
 object P081 {
   import commons._
+  import collection.mutable.PriorityQueue
 
-  def findMinRoute(matrix: Array[Array[Int]], i: Int, j: Int): Long = {
-    val rMax = matrix.length - 1
-    val dMax = matrix.length - 1
+  type Vertex = (Int, Int)
 
-    if (j == rMax) matrix.drop(i).map { _(j) }.sum
-    else if (i == dMax) matrix(i).drop(j).sum
-    else matrix(i)(j) + findMinRoute(matrix, i + 1, j).min(findMinRoute(matrix, i, j + 1))
+  def init(matrix: Array[Array[Int]]) =
+    PriorityQueue.empty[(Int, Vertex)](Ordering.by[(Int, Vertex), Int] { _._1 }.reverse) ++=
+      (0 to matrix.length - 1).flatMap { i =>
+        (0 to matrix(i).length - 1).map { j =>
+          val dv = if ((i, j) == (0, 0)) matrix(0)(0) else Int.MaxValue
+          (dv, (i, j))
+        }
+      }
+
+  def findMinRoute(matrix: Array[Array[Int]], queue: PriorityQueue[(Int, Vertex)], prev: Map[Vertex, Vertex]): Long = {
+    val iMax = matrix.length - 1
+    val jMax = matrix(0).length - 1
+
+    def next(u: Vertex) = u match {
+      case (i, j) if (i, j) == (iMax, jMax) => Nil
+      case (i, j) if i == iMax => List((i, j + 1))
+      case (i, j) if j == jMax => List((i + 1, j))
+      case (i, j) => List((i, j + 1), (i + 1, j))
+    }
+
+    def replace(q: PriorityQueue[(Int, Vertex)], u: Vertex, newVal: Int) = q.filter { case (_, v) => v != u } += ((newVal, u))
+
+    def sumRoute(prev: Map[Vertex, Vertex]) = {
+      def toList(p: Vertex): List[Int] = p match {
+        case (0, 0) => List(matrix(0)(0))
+        case (i, j) => matrix(i)(j) :: toList(prev(p))
+      }
+
+      toList((iMax, jMax)).sum
+    }
+
+    queue match {
+      case q if q.isEmpty => sumRoute(prev)
+      case q => {
+        val (du, u) = queue.dequeue
+        val (r, route) =
+          next(u)
+            .foldLeft((q, prev)) { case ((q, prev), (x, y)) =>
+              val alt = du + matrix(x)(y)
+              q.find { case (_, p) => p == (x, y) } match {
+                case Some((value, p)) if (value > alt) => (replace(q, p, alt), prev + (p -> u))
+                case _ => (q, prev)
+              }
+            }
+        findMinRoute(matrix, r, route)
+      }
+    }
   }
 
+  /**
+   * 最短経路問題なので、ダイクストラ法で解ける。
+   * 優先度付きキューを使って、
+   * ==================================================
+   * for (v ← V)
+   *   d(v) ← (v = sならば0、それ以外は∞)
+   *   prev(v) ← 無し
+   *   Q(v) ← d(v)
+   *
+   * while (Qが空集合でない)
+   *   QからQ(u)が最小である頂点uを取り出す
+   *   for each (uからの辺がある各 v ∈ V )
+   *     alt ← d(u) + length(u, v)
+   *     if (d(v) ＞ alt)
+   *       d(v) ← alt
+   *       prev(v) ← u
+   *       Q(v) ← alt
+   * ==================================================
+   * で解けるらしい。
+   */
   def solve: Long = {
     val matrix = withSource(io.Source.fromFile(new java.io.File("src/main/resources/p081_matrix.txt"))) { src =>
       src.getLines.map { _.split(",").map { _.toInt }.toArray }.toArray
     }
 
-    findMinRoute(matrix, 0, 0)
+    findMinRoute(matrix, init(matrix), Map.empty)
   }
 }
--- src/test/scala/p051_100/P081Spec.scala
+++ src/test/scala/p051_100/P081Spec.scala
@@ -4,33 +4,41 @@ import project_euler.P081
 import org.specs2.mutable._
 
 class P081Spec extends Specification {
+  import collection.mutable.PriorityQueue
+
+  val matrix = Array(
+    Array(131, 673, 234, 103, 18),
+    Array(201, 96, 342, 965, 150),
+    Array(630, 803, 746, 422, 111),
+    Array(537, 699, 497, 121, 956),
+    Array(805, 732, 524, 37, 331)
+  )
+
+  "P081#init(Array[Array[Int]])" should {
+    "returns [(131, (0, 0)), (Int.MaxValue, (0, 1)), ...] if matrix = <省略>" in {
+      val expected = PriorityQueue(
+        (131, (0, 0)), (Int.MaxValue, (0, 1)), (Int.MaxValue, (0, 2)), (Int.MaxValue, (0, 3)), (Int.MaxValue, (0, 4)),
+        (Int.MaxValue, (1, 0)), (Int.MaxValue, (1, 1)), (Int.MaxValue, (1, 2)), (Int.MaxValue, (1, 3)), (Int.MaxValue, (1, 4)),
+        (Int.MaxValue, (2, 0)), (Int.MaxValue, (2, 1)), (Int.MaxValue, (2, 2)), (Int.MaxValue, (2, 3)), (Int.MaxValue, (2, 4)),
+        (Int.MaxValue, (3, 0)), (Int.MaxValue, (3, 1)), (Int.MaxValue, (3, 2)), (Int.MaxValue, (3, 3)), (Int.MaxValue, (3, 4)),
+        (Int.MaxValue, (4, 0)), (Int.MaxValue, (4, 1)), (Int.MaxValue, (4, 2)), (Int.MaxValue, (4, 3)), (Int.MaxValue, (4, 4))
+      )(Ordering.by[(Int, (Int, Int)), Int] { _._1 }.reverse)
+
+      P081.init(matrix) must beEqualTo(expected)
+    }
+  }
+
   "P081#findMinRoute(Array[Array[Int]], Int, Int)" should {
-    val matrix = Array(
-      Array(131, 673, 234, 103, 18),
-      Array(201, 96, 342, 965, 150),
-      Array(630, 803, 746, 422, 111),
-      Array(537, 699, 497, 121, 956),
-      Array(805, 732, 524, 37, 331)
-    )
+    "returns 2427 if (matrix, queue) = (<省略>, <省略>)" in {
+      val queue = PriorityQueue(
+        (131, (0, 0)), (Int.MaxValue, (0, 1)), (Int.MaxValue, (0, 2)), (Int.MaxValue, (0, 3)), (Int.MaxValue, (0, 4)),
+        (Int.MaxValue, (1, 0)), (Int.MaxValue, (1, 1)), (Int.MaxValue, (1, 2)), (Int.MaxValue, (1, 3)), (Int.MaxValue, (1, 4)),
+        (Int.MaxValue, (2, 0)), (Int.MaxValue, (2, 1)), (Int.MaxValue, (2, 2)), (Int.MaxValue, (2, 3)), (Int.MaxValue, (2, 4)),
+        (Int.MaxValue, (3, 0)), (Int.MaxValue, (3, 1)), (Int.MaxValue, (3, 2)), (Int.MaxValue, (3, 3)), (Int.MaxValue, (3, 4)),
+        (Int.MaxValue, (4, 0)), (Int.MaxValue, (4, 1)), (Int.MaxValue, (4, 2)), (Int.MaxValue, (4, 3)), (Int.MaxValue, (4, 4))
+      )(Ordering.by[(Int, (Int, Int)), Int] { _._1 }.reverse)
 
-    "returns 331 if (matrix, i, j) = (<省略>, 4, 4)" in {
-      P081.findMinRoute(matrix, 4, 4) must beEqualTo(331)
-    }
-
-    "returns 1287 if (matrix, i, j) = (<省略>, 3, 4)" in {
-      P081.findMinRoute(matrix, 3, 4) must beEqualTo(1287)
-    }
-
-    "returns 368 if (matrix, i, j) = (<省略>, 4, 3)" in {
-      P081.findMinRoute(matrix, 4, 3) must beEqualTo(368)
-    }
-
-    "returns 489 if (matrix, i, j) = (<省略>, 3, 3)" in {
-      P081.findMinRoute(matrix, 3, 3) must beEqualTo(489)
-    }
-
-    "returns 2427 if (matrix, i, j) = (<省略>, 0, 0)" in {
-      P081.findMinRoute(matrix, 0, 0) must beEqualTo(2427)
+      P081.findMinRoute(matrix, queue, Map.empty) must beEqualTo(2427)
     }
   }
 
