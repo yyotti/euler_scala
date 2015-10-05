5fe832ddcb395c4a37f6098fab1d7e97259b2cc3
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-05 18:11:32 +0900

    Problem 83 への回答完了

--- src/main/scala/p051_100/P083.scala
+++ src/main/scala/p051_100/P083.scala
@@ -29,5 +29,55 @@ package project_euler
  * 今, 31Kのテキストファイルmatrix.txtには80×80の行列が書かれている. 上下左右に移動し左上のセルから開始し右下のセルで終了する道に沿った和の最小を求めよ.
  */
 object P083 {
-  def solve: Long = ???
+  import commons._
+  import collection.mutable.PriorityQueue
+
+  type Vertex = (Int, Int)
+
+  def init(matrix: Array[Array[Int]]) =
+    PriorityQueue.empty[(Int, Vertex)](Ordering.by[(Int, Vertex), Int] { _._1 }.reverse) ++=
+      (0 to matrix.length - 1).flatMap { i =>
+        (0 to matrix(i).length - 1).map { j =>
+          val dv = if ((i, j) == (0, 0)) matrix(i)(j) else Int.MaxValue
+          (dv, (i, j))
+        }
+      }
+
+  def findMinRoute(matrix: Array[Array[Int]], queue: PriorityQueue[(Int, Vertex)]): Long = {
+    val iMax = matrix.length - 1
+    val jMax = matrix(0).length - 1
+
+    def next(i: Int, j: Int) =
+      Seq((-1, 0), (1, 0), (0, -1), (0, 1))
+        .map { case (dy, dx) => (i + dy, j + dx) }
+        .filter { case (i, j) => i >= 0 && i <= iMax && j >= 0 && j <= jMax }
+
+    def replace(q: PriorityQueue[(Int, Vertex)], u: Vertex, newVal: Int) = q.filter { case (_, v) => v != u } += ((newVal, u))
+
+    val (du, (i, j)) = queue.dequeue
+    if ((i, j) == (iMax, jMax)) du
+    else {
+      val r =
+        next(i, j)
+          .foldLeft(queue) { case (q, (x, y)) =>
+            val alt = du + matrix(x)(y)
+            q.find { case (_, p) => p == (x, y) } match {
+              case Some((value, p)) if (value > alt) => replace(q, p, alt)
+              case _ => q
+            }
+          }
+      findMinRoute(matrix, r)
+    }
+  }
+
+  /**
+   * P082と同様、ダイクストラ法でやれる。
+   */
+  def solve: Long = {
+    val matrix = withSource(io.Source.fromFile(new java.io.File("src/main/resources/p083_matrix.txt"))) { src =>
+      src.getLines.map { _.split(",").map { _.toInt }.toArray }.toArray
+    }
+
+    findMinRoute(matrix, init(matrix))
+  }
 }
--- src/test/scala/p051_100/P083Spec.scala
+++ src/test/scala/p051_100/P083Spec.scala
@@ -4,6 +4,44 @@ import project_euler.P083
 import org.specs2.mutable._
 
 class P083Spec extends Specification {
+  import collection.mutable.PriorityQueue
+
+  val matrix = Array(
+    Array(131, 673, 234, 103, 18),
+    Array(201, 96, 342, 965, 150),
+    Array(630, 803, 746, 422, 111),
+    Array(537, 699, 497, 121, 956),
+    Array(805, 732, 524, 37, 331)
+  )
+
+  "P083#init(Array[Array[Int]])" should {
+    "returns [(131, (0, 0)), (Int.MaxValue, (0, 1)), ...] if matrix = <省略>" in {
+      val expected = PriorityQueue(
+        (131, (0, 0)), (Int.MaxValue, (0, 1)), (Int.MaxValue, (0, 2)), (Int.MaxValue, (0, 3)), (Int.MaxValue, (0, 4)),
+        (Int.MaxValue, (1, 0)), (Int.MaxValue, (1, 1)), (Int.MaxValue, (1, 2)), (Int.MaxValue, (1, 3)), (Int.MaxValue, (1, 4)),
+        (Int.MaxValue, (2, 0)), (Int.MaxValue, (2, 1)), (Int.MaxValue, (2, 2)), (Int.MaxValue, (2, 3)), (Int.MaxValue, (2, 4)),
+        (Int.MaxValue, (3, 0)), (Int.MaxValue, (3, 1)), (Int.MaxValue, (3, 2)), (Int.MaxValue, (3, 3)), (Int.MaxValue, (3, 4)),
+        (Int.MaxValue, (4, 0)), (Int.MaxValue, (4, 1)), (Int.MaxValue, (4, 2)), (Int.MaxValue, (4, 3)), (Int.MaxValue, (4, 4))
+      )(Ordering.by[(Int, (Int, Int)), Int] { _._1 }.reverse)
+
+      P083.init(matrix) must beEqualTo(expected)
+    }
+  }
+
+  "P083#findMinRoute(Array[Array[Int]], PriorityQueue[(Int, Vertex)])" should {
+    "returns 2297 if (matrix, queue) = (<省略>, <省略>)" in {
+      val queue = PriorityQueue(
+        (131, (0, 0)), (Int.MaxValue, (0, 1)), (Int.MaxValue, (0, 2)), (Int.MaxValue, (0, 3)), (Int.MaxValue, (0, 4)),
+        (Int.MaxValue, (1, 0)), (Int.MaxValue, (1, 1)), (Int.MaxValue, (1, 2)), (Int.MaxValue, (1, 3)), (Int.MaxValue, (1, 4)),
+        (Int.MaxValue, (2, 0)), (Int.MaxValue, (2, 1)), (Int.MaxValue, (2, 2)), (Int.MaxValue, (2, 3)), (Int.MaxValue, (2, 4)),
+        (Int.MaxValue, (3, 0)), (Int.MaxValue, (3, 1)), (Int.MaxValue, (3, 2)), (Int.MaxValue, (3, 3)), (Int.MaxValue, (3, 4)),
+        (Int.MaxValue, (4, 0)), (Int.MaxValue, (4, 1)), (Int.MaxValue, (4, 2)), (Int.MaxValue, (4, 3)), (Int.MaxValue, (4, 4))
+      )(Ordering.by[(Int, (Int, Int)), Int] { _._1 }.reverse)
+
+      P083.findMinRoute(matrix, queue) must beEqualTo(2297)
+    }
+  }
+
   "P083#solve" should {
     "returns 425185" in {
       P083.solve must beEqualTo(425185)
