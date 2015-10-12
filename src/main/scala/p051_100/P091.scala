fb670b8093966e22585d3d004ae4f71532df90c9
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-12 19:07:45 +0900

    Problem 91 への回答完了

--- src/main/scala/p051_100/P091.scala
+++ src/main/scala/p051_100/P091.scala
@@ -25,5 +25,45 @@ package project_euler
  * では, 0 ≤ x1, y1, x2, y2 ≤ 50のとき, 直角三角形は何個作れるか?
  */
 object P091 {
-  def solve(n: Int): Long = ???
+
+  def isRightTriangle(p: (Int, Int), q: (Int, Int)) = {
+    if (p == q) false
+    else if (p == (0, 0) || q == (0, 0)) false
+    else {
+      val (x1, y1) = p
+      val (x2, y2) = q
+      val (x3, y3) = (x2 - x1, y2 - y1)
+      (x1 * x2 + y1 * y2 == 0) || (x1 * x3 + y1 * y3 == 0) || (x2 * x3 + y2 * y3 == 0)
+    }
+  }
+
+  /**
+   * PとQの位置が入れ替わっても同じ三角形になるので、図のような位置関係を保つなら
+   *   x1 &le; x2
+   *   y1 &ge; y2
+   * としてよい。
+   * また、O,P,Qのいずれかが重なると三角形にならない。
+   *
+   * △OPQが直角三角形であるか否かは、OP, OQ, PQをそれぞれベクトルと捉えると、内積が0となる
+   * 組み合わせが存在するか否かである。
+   *   OP = (x1, y1)
+   *   OQ = (x2, y2)
+   *   PQ = OQ - OP = (x2 - x1, y2 - y1)
+   * であるから、それぞれの内積は
+   *   OP・OQ = x1*x2 + y1*y2
+   *   OP・PQ = x1*(x2 - x1) + y1*(y2 - y1)
+   *   OQ・PQ = x2*(x2 - x1) + y2*(y2 - y1)
+   * で求められるので、このいずれかが0となるものをカウントすればよい。
+   */
+  def solve(n: Int): Long = {
+    (0 to n).flatMap { x2 =>
+      (0 to x2).flatMap { x1 =>
+        (0 to n).flatMap { y1 =>
+          (0 to y1).map { y2 =>
+            isRightTriangle((x1, y1), (x2, y2))
+          }
+        }
+      }
+    }.count { _ == true }
+  }
 }
--- src/test/scala/p051_100/P091Spec.scala
+++ src/test/scala/p051_100/P091Spec.scala
@@ -4,6 +4,28 @@ import project_euler.P091
 import org.specs2.mutable._
 
 class P091Spec extends Specification {
+  "P091#isRightTriangle((Int, Int), (Int, Int))" should {
+    "returns true if (p, q) = ((1, 0), (0, 1))" in {
+      P091.isRightTriangle((1, 0), (0, 1)) must beTrue
+    }
+
+    "returns true if (p, q) = ((2, 0), (0, 1))" in {
+      P091.isRightTriangle((2, 0), (0, 1)) must beTrue
+    }
+
+    "returns true if (p, q) = ((1, 0), (0, 2))" in {
+      P091.isRightTriangle((1, 0), (0, 2)) must beTrue
+    }
+
+    "returns false if (p, q) = ((2, 1), (0, 2))" in {
+      P091.isRightTriangle((2, 1), (0, 2)) must beFalse
+    }
+
+    "returns true if (p, q) = ((1, 1), (2, 0))" in {
+      P091.isRightTriangle((1, 1), (2, 0)) must beTrue
+    }
+  }
+
   "P091#solve(Int)" should {
     "returns 14 if n = 2" in {
       P091.solve(2) must beEqualTo(14)
