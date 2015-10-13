840950e2dda8b17e0bb707a177196db4affb322f
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-13 16:09:10 +0900

    Problem 93 への回答完了

--- src/main/scala/p051_100/P093.scala
+++ src/main/scala/p051_100/P093.scala
@@ -38,5 +38,116 @@ package project_euler
  * 最長の連続した正の整数 1 から n の集合を得ることができる, 4 つの異なる数字 a ＜ b ＜ c ＜ d を見つけよ. 答えを文字列 abcd として与えよ.
  */
 object P093 {
-  def solve: String = ???
+
+  sealed abstract class FormulaElement {
+    val priority = -1
+  }
+  sealed abstract class Operator extends FormulaElement
+  case class Expr(ex: Expr) extends FormulaElement
+  case object ParenthesisS extends FormulaElement
+  case object ParenthesisE extends FormulaElement
+
+  case class Num(value: Int) extends FormulaElement
+  case object Plus extends Operator { override val priority = 0 }
+  case object Minus extends Operator { override val priority = 0 }
+  case object Times extends Operator { override val priority = 1 }
+  case object Div extends Operator { override val priority = 1 }
+
+  val operators = List(Plus, Minus, Times, Div)
+
+  def toRPN(formula: List[FormulaElement]): List[FormulaElement] = {
+    def conv(formula: List[FormulaElement], buff: Seq[FormulaElement], stack: List[FormulaElement]): List[FormulaElement] = formula match {
+      case Nil => (buff ++ stack).toList
+      case Num(x) :: xs => conv(xs, buff :+ Num(x), stack)
+      case ParenthesisE :: xs => conv(xs, buff ++ stack.takeWhile { _ != ParenthesisS }, stack.dropWhile { _ != ParenthesisS }.tail)
+      case ParenthesisS :: xs => conv(xs, buff, ParenthesisS :: stack)
+      case x :: xs if (stack.isEmpty) => conv(xs, buff, x :: stack)
+      case x :: _ if (stack.head.priority >= x.priority) => conv(formula, buff :+ stack.head, stack.tail)
+      case x :: xs => conv(xs, buff, x :: stack)
+    }
+
+    conv(formula, Seq.empty, Nil)
+  }
+
+  def calcRPN(formula: List[FormulaElement]): Double = {
+    def calc(formula: List[FormulaElement], stack: List[Double]): Double = (formula, stack) match {
+      case (Nil, _) => stack.head
+      case (Num(x) :: xs, _) => calc(xs, x :: stack)
+      case (Plus :: xs, m :: n :: ts) => calc(xs, (n + m) :: ts)
+      case (Minus :: xs, m :: n :: ts) => calc(xs, (n - m) :: ts)
+      case (Times :: xs, m :: n :: ts) => calc(xs, (n * m) :: ts)
+      case (Div :: xs, m :: n :: ts) => calc(xs, (n.toDouble / m) :: ts)
+      case _ => 0
+    }
+
+    calc(formula, Nil)
+  }
+
+  def insertOperators(nums: List[Num]): List[List[FormulaElement]] = nums match {
+    case Nil => List(Nil)
+    case n :: Nil => List(List(n))
+    case n :: ns => operators.flatMap { op => insertOperators(ns).map { xs => n :: op :: xs } }
+  }
+
+  def addParenthesis(formula: List[FormulaElement]): List[List[FormulaElement]] = {
+    def insertAt[FormulaElement](ls: List[FormulaElement], n: Int, elem: FormulaElement): List[FormulaElement] = (ls, n) match {
+      case (_, 0) => elem :: ls
+      case (Nil, _) => List(elem)
+      case (x :: xs, n) => x :: insertAt(xs, n - 1, elem)
+    }
+
+    // パターンが少ないのでハードコーディング
+    List(
+      formula,
+      insertAt(insertAt(formula, 0, ParenthesisS), 4, ParenthesisE),
+      insertAt(insertAt(formula, 2, ParenthesisS), 6, ParenthesisE),
+      insertAt(insertAt(formula, 4, ParenthesisS), 8, ParenthesisE),
+      insertAt(insertAt(insertAt(insertAt(formula, 0, ParenthesisS), 4, ParenthesisE), 6, ParenthesisS), 10, ParenthesisE),
+      insertAt(insertAt(formula, 0, ParenthesisS), 6, ParenthesisE),
+      insertAt(insertAt(formula, 2, ParenthesisS), 8, ParenthesisE),
+      insertAt(insertAt(insertAt(insertAt(formula, 0, ParenthesisS), 1, ParenthesisS), 5, ParenthesisE), 8, ParenthesisE),
+      insertAt(insertAt(insertAt(insertAt(formula, 0, ParenthesisS), 3, ParenthesisS), 7, ParenthesisE), 8, ParenthesisE),
+      insertAt(insertAt(insertAt(insertAt(formula, 2, ParenthesisS), 3, ParenthesisS), 7, ParenthesisE), 10, ParenthesisE),
+      insertAt(insertAt(insertAt(insertAt(formula, 2, ParenthesisS), 5, ParenthesisS), 9, ParenthesisE), 10, ParenthesisE)
+    )
+  }
+
+  def findN(nums: List[Int]): Int =
+    nums
+      .map { n => Num(n) }
+      .permutations
+      .toSeq
+      .flatMap { ns =>
+        insertOperators(ns).flatMap { formula =>
+          addParenthesis(formula).map { f => calcRPN(toRPN(f)) }
+        }
+      }
+      .filter { k => k > 0 && k.toInt == k }
+      .sorted
+      .distinct
+      .zip(Stream.from(1))
+      .takeWhile { case (n1, n2) => n1 == n2 }
+      .size
+
+  /**
+   * 全パターンを網羅する場合を考える。
+   *   9つの数字から4つを選ぶ場合の選び方： 9C4 = 126 通り
+   *   4つの数字の並べ方： 4! = 24 通り
+   *   4つの数字の間(3箇所)に4つの演算子を入れる場合の入れ方： 4^3 = 64 通り
+   *   括弧で囲む場合の囲み方： 全く囲まない → 1通り
+   *                            2つの数字を括弧で囲む → 4 通り
+   *                            3つの数字を括弧で囲む → 2 通り
+   *                            3つの数字を囲みつつその中の2つの数字を括弧で囲む → 4 通り
+   *                            合計 11 通り
+   * よってパターンの総数は 126 * 24 * 64 * 11 = 2,128,896 通りある。
+   *
+   * 手順は
+   *   1. 9つの数字から4つを選択する
+   *   2. 選択した数字と演算子、および括弧の組み合わせで生成できる正の整数を全て列挙し、重複を排除して小さい順に並べる。
+   *   3. 1から始めて連続した整数がいくつまで生成できているかチェックし、最大値をnとする。1が生成できていない場合はn = 0とする。
+   *   4. 最も大きなnを得られた4つの数字を小さい順に連結し、解とする。
+   *
+   * 括弧を含む演算があるので、式を逆ポーランド記法に変換して計算する。
+   */
+  def solve: String = (1 to 9).combinations(4).maxBy { ls => findN(ls.toList) }.mkString
 }
--- src/test/scala/p051_100/P093Spec.scala
+++ src/test/scala/p051_100/P093Spec.scala
@@ -1,9 +1,113 @@
 package test.project_euler
 
 import project_euler.P093
+import project_euler.P093.{Num, Plus, Minus, Times, Div, ParenthesisS, ParenthesisE}
 import org.specs2.mutable._
 
 class P093Spec extends Specification {
+  "P093#toRPN(List[FormulaElement])" should {
+    "returns [4, 1, 3, +, *, 2, /] if formula = [(, 4, *, (, 1, +, 3, ), ), /, 2]" in {
+      P093.toRPN(List(ParenthesisS, Num(4), Times, ParenthesisS, Num(1), Plus, Num(3), ParenthesisE, ParenthesisE, Div, Num(2))) must beEqualTo(List(Num(4), Num(1), Num(3), Plus, Times, Num(2), Div))
+    }
+
+    "returns [4, 3, 1, 2, /, +, *] if formula = [4, *, (, 3, +, 1, /, 2, )]" in {
+      P093.toRPN(List(Num(4), Times, ParenthesisS, Num(3), Plus, Num(1), Div, Num(2), ParenthesisE)) must beEqualTo(List(Num(4), Num(3), Num(1), Num(2), Div, Plus, Times))
+    }
+
+    "returns [4, 2, 3, +, *, 1, -] if formula = [4, *, (, 2, +, 3, ), -, 1]" in {
+      P093.toRPN(List(Num(4), Times, ParenthesisS, Num(2), Plus, Num(3), ParenthesisE, Minus, Num(1))) must beEqualTo(List(Num(4), Num(2), Num(3), Plus, Times, Num(1), Minus))
+    }
+
+    "returns [3, 4, *, 2, 1, +, *] if formula = [3, *, 4, *, (, 2, +, 1, )]" in {
+      P093.toRPN(List(Num(3), Times, Num(4), Times, ParenthesisS, Num(2), Plus, Num(1), ParenthesisE)) must beEqualTo(List(Num(3), Num(4), Times, Num(2), Num(1), Plus, Times))
+    }
+  }
+
+  "P093#calcRPN(List[FormulaElement])" should {
+    "returns 8 if formula = [4, 1, 3, +, *, 2, /]" in {
+      P093.calcRPN(List(Num(4), Num(1), Num(3), Plus, Times, Num(2), Div)) must beEqualTo(8)
+    }
+
+    "returns 14 if formula = [4, 3, 1, 2, /, +, *]" in {
+      P093.calcRPN(List(Num(4), Num(3), Num(1), Num(2), Div, Plus, Times)) must beEqualTo(14)
+    }
+
+    "returns 19 if formula = [4, 2, 3, +, *, 1, -]" in {
+      P093.calcRPN(List(Num(4), Num(2), Num(3), Plus, Times, Num(1), Minus)) must beEqualTo(19)
+    }
+
+    "returns 36 if formula = [3, 4, *, 2, 1, +, *]" in {
+      P093.calcRPN(List(Num(3), Num(4), Times, Num(2), Num(1), Plus, Times)) must beEqualTo(36)
+    }
+  }
+
+  "P093#insertOperators(List[Num])" should {
+    "returns [[]] if nums = []" in {
+      P093.insertOperators(Nil) must beEqualTo(List(Nil))
+    }
+
+    "returns [[1]] if nums = [1]" in {
+      P093.insertOperators(List(Num(1))) must beEqualTo(List(List(Num(1))))
+    }
+
+    "returns [[1, +, 2], [1, -, 2], [1, *, 2], [1, /, 2]] if nums = [1, 2]" in {
+      val expected = List(
+        List(Num(1), Plus, Num(2)),
+        List(Num(1), Minus, Num(2)),
+        List(Num(1), Times, Num(2)),
+        List(Num(1), Div, Num(2))
+      )
+      P093.insertOperators(List(Num(1), Num(2))) must beEqualTo(expected)
+    }
+
+    "returns [[1, +, 2, +, 3], [1, +, 2, -, 3], ...] if nums = [1, 2, 3]" in {
+      val expected = List(
+        List(Num(1), Plus, Num(2), Plus, Num(3)),
+        List(Num(1), Plus, Num(2), Minus, Num(3)),
+        List(Num(1), Plus, Num(2), Times, Num(3)),
+        List(Num(1), Plus, Num(2), Div, Num(3)),
+        List(Num(1), Minus, Num(2), Plus, Num(3)),
+        List(Num(1), Minus, Num(2), Minus, Num(3)),
+        List(Num(1), Minus, Num(2), Times, Num(3)),
+        List(Num(1), Minus, Num(2), Div, Num(3)),
+        List(Num(1), Times, Num(2), Plus, Num(3)),
+        List(Num(1), Times, Num(2), Minus, Num(3)),
+        List(Num(1), Times, Num(2), Times, Num(3)),
+        List(Num(1), Times, Num(2), Div, Num(3)),
+        List(Num(1), Div, Num(2), Plus, Num(3)),
+        List(Num(1), Div, Num(2), Minus, Num(3)),
+        List(Num(1), Div, Num(2), Times, Num(3)),
+        List(Num(1), Div, Num(2), Div, Num(3))
+      )
+      P093.insertOperators(List(Num(1), Num(2), Num(3))) must beEqualTo(expected)
+    }
+  }
+
+  "P093#addParenthesis(List[FormulaElement])" should {
+    "returns [[1, +, 2, -, 3, *, 4], [(, 1, +, 2, ), -, 3, *, 4], ...] if nums = [1, +, 2, -, 3, *, 4]" in {
+      val expected = List(
+        List(Num(1), Plus, Num(2), Minus, Num(3), Times, Num(4)),
+        List(ParenthesisS, Num(1), Plus, Num(2), ParenthesisE, Minus, Num(3), Times, Num(4)),
+        List(Num(1), Plus, ParenthesisS, Num(2), Minus, Num(3), ParenthesisE, Times, Num(4)),
+        List(Num(1), Plus, Num(2), Minus, ParenthesisS, Num(3), Times, Num(4), ParenthesisE),
+        List(ParenthesisS, Num(1), Plus, Num(2), ParenthesisE, Minus, ParenthesisS, Num(3), Times, Num(4), ParenthesisE),
+        List(ParenthesisS, Num(1), Plus, Num(2), Minus, Num(3), ParenthesisE, Times, Num(4)),
+        List(Num(1), Plus, ParenthesisS, Num(2), Minus, Num(3), Times, Num(4), ParenthesisE),
+        List(ParenthesisS, ParenthesisS, Num(1), Plus, Num(2), ParenthesisE, Minus, Num(3), ParenthesisE, Times, Num(4)),
+        List(ParenthesisS, Num(1), Plus, ParenthesisS, Num(2), Minus, Num(3), ParenthesisE, ParenthesisE, Times, Num(4)),
+        List(Num(1), Plus, ParenthesisS, ParenthesisS, Num(2), Minus, Num(3), ParenthesisE, Times, Num(4), ParenthesisE),
+        List(Num(1), Plus, ParenthesisS, Num(2), Minus, ParenthesisS, Num(3), Times, Num(4), ParenthesisE, ParenthesisE)
+      )
+      P093.addParenthesis(List(Num(1), Plus, Num(2), Minus, Num(3), Times, Num(4))) must beEqualTo(expected)
+    }
+  }
+
+  "P093#findN(List[Int])" should {
+    "returns 28 if nums = [1, 2, 3, 4]" in {
+      P093.findN(List(1, 2, 3, 4)) must beEqualTo(28)
+    }
+  }
+
   "P093#solve(Int)" should {
     """returns "1258"""" in {
       P093.solve must beEqualTo("1258")
