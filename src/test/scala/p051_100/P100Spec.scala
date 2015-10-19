04464f83fbb6319ed9c0532cb572c0fa859b9d32
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-19 19:34:11 +0900

    Problem 100 への回答完了

--- src/test/scala/p051_100/P100Spec.scala
+++ src/test/scala/p051_100/P100Spec.scala
@@ -4,6 +4,12 @@ import project_euler.P100
 import org.specs2.mutable._
 
 class P100Spec extends Specification {
+  "P100#pell" should {
+    "first 10 terms are [(1, 1), (7, 5), (41, 29), (239, 169), (1393, 985), (8119, 5741), (47321, 33461), (275807, 195025), (1607521, 1136689), (9369319, 6625109)]" in {
+      P100.pell.take(10).toList must beEqualTo(List((1, 1), (7, 5), (41, 29), (239, 169), (1393, 985), (8119, 5741), (47321, 33461), (275807, 195025), (1607521, 1136689), (9369319, 6625109)))
+    }
+  }
+
   "P100#solve(Int)" should {
     "returns 85" in {
       P100.solve(100) must beEqualTo(85)
