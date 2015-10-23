cfd19b02c3d2a02c0f2014b31884c30be5a1fa03
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-23 16:28:49 +0900

    Problem 101 への回答完了

--- src/main/scala/p101_150/P101.scala
+++ src/main/scala/p101_150/P101.scala
@@ -71,5 +71,80 @@ package project_euler
  * BOPのFITの総和を求めよ.
  */
 object P101 {
-  def solve(n: Int): Long = ???
+
+  val u = Stream.from(1).map { n => (1 - math.pow(-n, 11).toLong) / (1 + n) }
+
+  def op(k: Int, n: Int) =
+    (0 until k)
+      .zip(coefficients(k))
+      .map { case (i, a) => a * math.pow(n, i).toLong }
+      .sum
+
+  def coefficients(k: Int): List[Long] = {
+    val coefMatrix =
+      (1 to k)
+        .map { n => (0 until k).map { i => math.pow(n, i).toLong }.toList }
+        .toList
+
+    gaussJordanElimination(coefMatrix, u.take(k).toList)._2
+  }
+
+  def gaussJordanElimination(a: List[List[Long]], b: List[Long]): (List[List[Long]], List[Long]) = {
+    def loop(a: List[List[Long]], b: List[Long], i: Int): (List[List[Long]], List[Long]) =
+      if (i == a.size) (a, b)
+      else if (a(i)(i) != 1) {
+        val x = a(i)(i)
+        loop(a.updated(i, a(i).map { _ / x }), b.updated(i, b(i) / x), i)
+      } else {
+        val ls = a(i)
+        val (newA, newB) =
+          (0 until a.size)
+            .filter { j => j != i }
+            .foldLeft((a, b)) { case ((a, b), j) =>
+              val s = a(j)(i)
+              (a.updated(j, a(j).zip(ls).map { case (aj, ai) => aj - ai * s }), b.updated(j, b(j) - b(i) * s))
+            }
+
+        loop(newA, newB, i + 1)
+      }
+
+    loop(a, b, 0)
+  }
+
+  def fit(k: Int) = {
+    val f = op(k, _: Int)
+    u
+      .zipWithIndex
+      .map { case (un, i) => (un, f(i + 1)) }
+      .find { case (un, t) => un != t }
+      .map { _._2 }
+      .get
+  }
+
+  /**
+   * OP(k, n)は下記のように記述できる。
+   *   OP(k, n) = Σ(i = 0 → k - 1) { a(i)n^i }  (a(i)は実数)
+   *
+   * OP(k, n)の係数列 a(n) を考える。
+   * k = 1のとき
+   *   OP(1, n) = Σ(i = 0 → 0) { a(i)n^i } = a(0)
+   * k = 2のとき
+   *   OP(2, n) = Σ(i = 0 → 1) { a(i)n^i } = a(0) + a(1)n
+   * k = 3のとき
+   *   OP(3, n) = Σ(i = 0 → 2) { a(i)n^i } = a(0) + a(1)n + a(2)n^2
+   * と続いていく。
+   * あるkについて、1 &le; n &le; k の全ての場合を列挙するとk元一次連立方程式となり、係数列 a(n) の
+   * 全ての項を特定することができる。
+   *
+   * a(n) を特定できればOP(k, n)の値を計算できるようになるので、BOPのFITを列挙して総和をとればよい。
+   *
+   * 連立方程式は行列式で解くことができる。
+   * 方法としては、逆行列を用いる正攻法やクラメルの定理などあるようだが、今回は対象とする連立方程式
+   * の性質上、ガウス-ジョルダンの消去法を採用する。
+   *
+   * u(n)自体は、初項が1、公比が-nの等比数列の第11項までの和なので、
+   *   u(n) = (1 - (-n)^10)/(1 + n)
+   * で求められる。
+   */
+  def solve(n: Int): Long = (1 to n).map { k => fit(k) }.sum
 }
--- src/test/scala/p101_150/P101Spec.scala
+++ src/test/scala/p101_150/P101Spec.scala
@@ -4,7 +4,158 @@ import project_euler.P101
 import org.specs2.mutable._
 
 class P101Spec extends Specification {
-  "P101#solve" should {
+  "P101#u" should {
+    "first 10 terms are [1, 683, 44287, 838861, 8138021, 51828151, 247165843, 954437177, 3138105961, 9090909091]" in {
+      P101.u.take(10).toList must beEqualTo(List(1, 683, 44287, 838861, 8138021, 51828151, 247165843, 954437177, 3138105961L, 9090909091L))
+    }
+  }
+
+  "P101#op(Int, Int)" should {
+    "returns 1 if (k, n) = (1, 1)" in {
+      P101.op(1, 1) must beEqualTo(1)
+    }
+
+    "returns 1 if (k, n) = (1, 2)" in {
+      P101.op(1, 2) must beEqualTo(1)
+    }
+
+    "returns 1 if (k, n) = (2, 1)" in {
+      P101.op(2, 1) must beEqualTo(1)
+    }
+
+    "returns 683 if (k, n) = (2, 2)" in {
+      P101.op(2, 2) must beEqualTo(683)
+    }
+
+    "returns 1365 if (k, n) = (2, 3)" in {
+      P101.op(2, 3) must beEqualTo(1365)
+    }
+
+    "returns 1 if (k, n) = (3, 1)" in {
+      P101.op(3, 1) must beEqualTo(1)
+    }
+
+    "returns 683 if (k, n) = (3, 2)" in {
+      P101.op(3, 2) must beEqualTo(683)
+    }
+
+    "returns 44287 if (k, n) = (3, 3)" in {
+      P101.op(3, 3) must beEqualTo(44287)
+    }
+
+    "returns 130813 if (k, n) = (3, 4)" in {
+      P101.op(3, 4) must beEqualTo(130813)
+    }
+
+    "returns 1 if (k, n) = (4, 1)" in {
+      P101.op(4, 1) must beEqualTo(1)
+    }
+
+    "returns 683 if (k, n) = (4, 2)" in {
+      P101.op(4, 2) must beEqualTo(683)
+    }
+
+    "returns 44287 if (k, n) = (4, 3)" in {
+      P101.op(4, 3) must beEqualTo(44287)
+    }
+
+    "returns 838861 if (k, n) = (4, 4)" in {
+      P101.op(4, 4) must beEqualTo(838861)
+    }
+
+    "returns 3092453 if (k, n) = (4, 5)" in {
+      P101.op(4, 5) must beEqualTo(3092453)
+    }
+  }
+
+  "P101#coefficients(Int)" should {
+    "returns [1] if k = 1" in {
+      P101.coefficients(1) must beEqualTo(List(1))
+    }
+
+    "returns [-681, 682] if k = 2" in {
+      P101.coefficients(2) must beEqualTo(List(-681, 682))
+    }
+
+    "returns [42241, -63701, 21461] if k = 3" in {
+      P101.coefficients(3) must beEqualTo(List(42241, -63701, 21461))
+    }
+
+    "returns [-665807, 1234387, -686587, 118008] if k = 4" in {
+      P101.coefficients(4) must beEqualTo(List(-665807, 1234387, -686587, 118008))
+    }
+  }
+
+  "P101#gaussJordanElimination(List[List[Long]], List[Long])" should {
+    "returns ([[1]], [1]) if (a, b) = ([[1]], [1])" in {
+      val a = List(
+        List(1L)
+      )
+      val b = List(1L)
+
+      val expectedA = List(
+        List(1)
+      )
+      val expectedB = List(1)
+      P101.gaussJordanElimination(a, b) must beEqualTo(expectedA, expectedB)
+    }
+
+    "returns (E, [-681, 682]) if (a, b) = (<省略>, [1, 683])" in {
+      val a = List(
+        List(1L, 1),
+        List(1L, 2)
+      )
+      val b = List(1L, 683)
+
+      val expectedA = List(
+        List(1, 0),
+        List(0, 1)
+      )
+      val expectedB = List(-681, 682)
+      P101.gaussJordanElimination(a, b) must beEqualTo(expectedA, expectedB)
+    }
+
+    "returns (E, [42241, -63701, 21461]) if (a, b) = (<省略>, [1, 683, 44287])" in {
+      val a = List(
+        List(1L, 1, 1),
+        List(1L, 2, 4),
+        List(1L, 3, 9)
+      )
+      val b = List(1L, 683, 44287)
+
+      val expectedA = List(
+        List(1, 0, 0),
+        List(0, 1, 0),
+        List(0, 0, 1)
+      )
+      val expectedB = List(42241, -63701, 21461)
+      P101.gaussJordanElimination(a, b) must beEqualTo(expectedA, expectedB)
+    }
+  }
+
+  "P101#fit(Int)" should {
+    "returns 1 if k = 1" in {
+      P101.fit(1) must beEqualTo(1)
+    }
+
+    "returns 1365 if k = 2" in {
+      P101.fit(2) must beEqualTo(1365)
+    }
+
+    "returns 130813 if k = 3" in {
+      P101.fit(3) must beEqualTo(130813)
+    }
+
+    "returns 3092453 if k = 4" in {
+      P101.fit(4) must beEqualTo(3092453)
+    }
+  }
+
+  "P101#solve(Int)" should {
+    "returns 3224632" in {
+      P101.solve(4) must beEqualTo(3224632)
+    }
+
     "returns 37076114526" in {
       P101.solve(10) must beEqualTo(37076114526L)
     }
