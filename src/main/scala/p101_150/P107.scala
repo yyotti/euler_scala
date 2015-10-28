7498895de08efa50f203394461bf63d6d7fb6f38
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-10-28 11:53:33 +0900

    Problem 107 への回答完了

--- src/main/scala/p101_150/P107.scala
+++ src/main/scala/p101_150/P107.scala
@@ -51,5 +51,79 @@ package project_euler
  * ネットワーク全体が連結であるが冗長な辺を取り除いたときの節約量を最大にするようにした場合の節約量を答えよ.
  */
 object P107 {
-  def solve: Long = ???
+  import commons._
+  import collection.mutable.PriorityQueue
+
+  case class Edge(from: Int, to: Int, weight: Int) extends Ordered[Edge] {
+    def compare(that: Edge): Int = that.weight - weight
+  }
+
+  def prim(graph: Map[Int, List[Edge]]): List[Edge] = {
+    val q = PriorityQueue(Edge(-1, 1, 0))
+
+    def loop(r: List[Edge], vd: Set[Int]): List[Edge] =
+      if (q.isEmpty) r
+      else {
+        val e = q.dequeue
+        if (vd.contains(e.to)) loop(r, vd)
+        else {
+          graph.get(e.to).map {
+            _.foreach {
+              case ne if ne.to != e.from => q.enqueue(ne)
+              case _ =>
+            }
+          }
+          loop(e :: r, vd + e.to)
+        }
+      }
+
+    loop(Nil, Set.empty).init
+  }
+
+  def toGraph(matrix: Array[Array[Int]]): Map[Int, List[Edge]] =
+    (0 until matrix.length).flatMap { i =>
+      (0 until matrix(i).length).flatMap { j =>
+        if (matrix(i)(j) == 0) None
+        else Some(Edge(i, j, matrix(i)(j)))
+      }
+    }.foldLeft(Map[Int, List[Edge]]()) { (map, e) => map + (e.from -> (e :: map.getOrElse(e.from, Nil))) }
+
+  /**
+   * ちょっとググってみたら、こういう問題は「重み付き連結グラフの最小全域木を求める最適化問題」なので
+   * 「プリム法」もしくは「クラスカル法」というアルゴリズムで解けるらしい。
+   * 今回はプリム法でやってみる。
+   *
+   * 入力：重み付きグラフ(頂点集合V、辺集合E)
+   * 出力：VnewとEnewが最小全域木を表している。
+   *   Vnew = {x}、ここでxはVの元であり、任意のノード(出発点)
+   *   Enew = {}
+   *   while (Vnew ≠ V)
+   *     Vnewに含まれる頂点uと含まれない頂点vを結ぶ重みが最小の辺(u,v)をEから選択
+   *     vをVnewに加える
+   *     (u,v)をEnewに加える
+   *
+   * 今回対象とするグラフは無向グラフなので、行列表現は対角要素を軸に対称性がある。
+   * しかし有向グラフと見たほうが実装が楽らしいので、半分ではなく全体を見る。
+   *
+   * 問題にある最適化後のネットワークを行列で表現すると下記のようになる。
+   *
+   *    A  B  C  D  E  F  G
+   * A  - 16 12  -  -  -  -
+   * B 16  -  - 17  -  -  -
+   * C 12  -  -  -  -  -  -
+   * D  - 17  -  - 18 19  -
+   * E  -  -  - 18  -  - 11
+   * F  -  -  - 19  -  -  -
+   * G  -  -  -  - 11  -  -
+   */
+  def solve: Long = {
+    val matrix = withSource(io.Source.fromFile(new java.io.File("src/main/resources/p107_network.txt"))) { src =>
+      src
+        .getLines
+        .map { line => line.split(",").map { s => if (s == "-") 0 else s.toInt }.toArray }
+        .toArray
+    }
+
+    matrix.map { _.sum }.sum / 2 - prim(toGraph(matrix)).map { _.weight }.sum
+  }
 }
--- src/test/scala/p101_150/P107Spec.scala
+++ src/test/scala/p101_150/P107Spec.scala
@@ -1,9 +1,63 @@
 package test.project_euler
 
 import project_euler.P107
+import project_euler.P107.Edge
 import org.specs2.mutable._
 
 class P107Spec extends Specification {
+  import collection.mutable.PriorityQueue
+
+  "P107#prim(Graph)" should {
+    "returns <省略> if graph = <省略>" in {
+      val graph = Map(
+        0 -> List(Edge(0, 3, 21), Edge(0, 2, 12), Edge(0, 1, 16)),
+        1 -> List(Edge(1, 4, 20), Edge(1, 3, 17), Edge(1, 0, 16)),
+        2 -> List(Edge(2, 5, 31), Edge(2, 3, 28), Edge(2, 0, 12)),
+        3 -> List(Edge(3, 6, 23), Edge(3, 5, 19), Edge(3, 4, 18), Edge(3, 2, 28), Edge(3, 1, 17), Edge(3, 0, 21)),
+        4 -> List(Edge(4, 6, 11), Edge(4, 3, 18), Edge(4, 1, 20)),
+        5 -> List(Edge(5, 6, 27), Edge(5, 3, 19), Edge(5, 2, 31)),
+        6 -> List(Edge(6, 5, 27), Edge(6, 4, 11), Edge(6, 3, 23))
+      )
+
+      val expected = List(
+        Edge(3, 5, 19),
+        Edge(4, 6, 11),
+        Edge(3, 4, 18),
+        Edge(1, 3, 17),
+        Edge(0, 2, 12),
+        Edge(1, 0, 16)
+      )
+
+      P107.prim(graph) must beEqualTo(expected)
+    }
+  }
+
+  "P107#toGraph(Array[Array[Int]])" should {
+    "returns <省略> if matrix = <省略>" in {
+      val matrix = Array(
+        Array( 0, 16, 12, 21,  0,  0,  0),
+        Array(16,  0,  0, 17, 20,  0,  0),
+        Array(12,  0,  0, 28,  0, 31,  0),
+        Array(21, 17, 28,  0, 18, 19, 23),
+        Array( 0, 20,  0, 18,  0,  0, 11),
+        Array( 0,  0, 31, 19,  0,  0, 27),
+        Array( 0,  0,  0, 23, 11, 27,  0)
+      )
+
+      val expected = Map(
+        0 -> List(Edge(0, 3, 21), Edge(0, 2, 12), Edge(0, 1, 16)),
+        1 -> List(Edge(1, 4, 20), Edge(1, 3, 17), Edge(1, 0, 16)),
+        2 -> List(Edge(2, 5, 31), Edge(2, 3, 28), Edge(2, 0, 12)),
+        3 -> List(Edge(3, 6, 23), Edge(3, 5, 19), Edge(3, 4, 18), Edge(3, 2, 28), Edge(3, 1, 17), Edge(3, 0, 21)),
+        4 -> List(Edge(4, 6, 11), Edge(4, 3, 18), Edge(4, 1, 20)),
+        5 -> List(Edge(5, 6, 27), Edge(5, 3, 19), Edge(5, 2, 31)),
+        6 -> List(Edge(6, 5, 27), Edge(6, 4, 11), Edge(6, 3, 23))
+      )
+
+      P107.toGraph(matrix) must beEqualTo(expected)
+    }
+  }
+
   "P107#solve" should {
     "returns 259679" in {
       P107.solve must beEqualTo(259679)
